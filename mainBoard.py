9c7ba022c99a094ce729216ef5bd7c2c7343ceb7
CleanestMink126 <steelman@students.olin.edu>
2017-02-27 18:50:27 -0500

    added thinf

--- mainBoard.py
+++ mainBoard.py
@@ -1,27 +1,56 @@
 """ Experiment with face detection and image filtering using OpenCV """
+# import shipClass
 
-import cv2
-import numpy as np
+import os
+import sys
+import pygame
+from pygame.locals import *
 
-cap = cv2.VideoCapture(0)
-if not cap.isOpened():
-    print('not working')
-#     cap.open()
-# print(cap.isOpened())
-while cap.isOpened():
-    # Capture frame-by-frame
+if not pygame.font:
+    print('Warning, fonts disabled')
+if not pygame.mixer:
+    print('Warning, sound disabled')
 
-    ret, frame = cap.read()
-    ret = cap.set(3, 320)
-    ret = cap.set(4, 240)
-    # Display the resulting frame
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
-    cv2.imshow('frame', gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+class BoardInit:
+    def __init__(self):
+        pygame.init()
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+        size = width, height = 1000, 1000
+        speed = [2, 2]
+        black = 0, 0, 0
+
+        screen = pygame.display.set_mode(size)
+        ball = pygame.draw.circle(screen, (255, 0, 0), (10, 10), 10, 0)
+
+        ship = pygame.image.load(os.path.join('rocket.png'))
+        shiprect = ship.get_rect()
+        while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    return
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    return
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    return
+            #ball = ball.move(speed)
+            shiprect = shiprect.move(speed)
+            if ball.left < 0 or ball.right > width:
+                speed[0] = -speed[0]
+            if ball.top < 0 or ball.bottom > height:
+                speed[1] = -speed[1]
+
+            screen.fill(black)
+
+            screen.blit(ship, shiprect)
+            #pygame.draw.circle(screen, (255, 0, 0), (10, 10), 10, 0)
+            pygame.display.flip()
+        pygame.display.quit()
+        pygame.quit()
+        sys.exit()
+
+
+if __name__ == '__main__':
+    board = BoardInit()
