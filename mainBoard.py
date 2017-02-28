e7533d31c023b1f4251b3cbb15b5a74cc1a50dbb
CleanestMink126 <steelman@students.olin.edu>
2017-02-28 15:13:13 -0500

    added the black hole

--- mainBoard.py
+++ mainBoard.py
@@ -13,51 +13,79 @@ if not pygame.mixer:
 
 
 class BoardInit:
+    def rotShip(self, myangle):
+        self.mainShip.rotatecw(angle=myangle)
+        self.ship = rot_center(self.shipImage, self.mainShip.get_angle_from_vert())
+        self.shiprect = self.ship.get_rect()
+        self.shiprect = self.shiprect.move(self.mainShip.pos[0], self.mainShip.pos[1])
+
+    def accelerate(self):
+        self.mainShip.speed[0] -= (self.mainShip.direct[0])
+        self.mainShip.speed[1] -= (self.mainShip.direct[1])
+
+    def moveShip(self):
+        myTime = self.detTime()
+        xc = self.mainShip.speed[0]
+        yc = self.mainShip.speed[1]
+        self.shiprect = self.shiprect.move(int(xc * myTime), int(yc * myTime))
+        self.mainShip.pos[0] += int(xc * myTime)
+        self.mainShip.pos[1] += int(yc * myTime)
+
+    def gravity(self):
+        distance = self.distanceCent()
+        self.mainShip.speed[0] -= ((self.mainShip.pos[0]-self.center[0])/distance * .3)
+        self.mainShip.speed[1] -= ((self.mainShip.pos[1]-self.center[1])/distance * .3)
+
+    def distanceCent(self):
+        return ((self.mainShip.pos[0]-self.center[0]) ** 2 + (self.mainShip.pos[1] - self.center[1]) ** 2) ** .5
+
+    def detTime(self):
+        distance = self.distanceCent()
+        print(distance)
+        return distance / (distance + 500)
+
+    def checkBounds(self):
+        if self.shiprect.left < 0 or self.shiprect.right > self.width:
+            self.mainShip.speed[0] = 0
+        if self.shiprect.top < 0 or self.shiprect.bottom > self.height:
+            self.mainShip.speed[1] = 0
+
     def __init__(self):
         pygame.init()
-        size = width, height = 1000, 1000
+        self.size = self.width, self.height = 1800, 1000
         black = 0, 0, 0
-        screen = pygame.display.set_mode(size)
-        # ball = pygame.draw.circle(screen, (255, 0, 0), (10, 10), 10, 0)
-        mainShip = shipClass.Ship(direct = [0,1], speed = [0,0], pos = [500,500])
+        self.screen = pygame.display.set_mode(self.size)
+        # ball = pygame.draw.circle(self.screen, (255, 0, 0), (10, 10), 10, 0)
+        self.mainShip = shipClass.Ship(direct = [0,1], speed = [0,0], pos = [500,500])
+        self.center = [self.width/2, self.height/2]
 
-
-        shipImage = pygame.image.load(os.path.join('rocket.png'))
-        shipImage = pygame.transform.scale(shipImage, (50, 50))
-        shipImage = pygame.transform.rotate(shipImage, -45)
-        ship = shipImage.copy()
-        shiprect = ship.get_rect()
-        shiprect = shiprect.move(mainShip.pos)
+        self.shipImage = pygame.image.load(os.path.join('rocket.png'))
+        self.shipImage = pygame.transform.scale(self.shipImage, (50, 50))
+        self.shipImage = pygame.transform.rotate(self.shipImage, -45)
+        self.ship = self.shipImage.copy()
+        self.shiprect = self.ship.get_rect()
+        self.shiprect = self.shiprect.move(self.mainShip.pos)
         while 1:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
                 elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     return
-                elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                    mainShip.speed[0] -= int(mainShip.direct[0] * 10)
-                    mainShip.speed[1] -= int(mainShip.direct[1] * 10)
-                elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
-                    mainShip.rotatecw(angle = 5.0)
-                    ship = rot_center(shipImage, mainShip.get_angle_from_vert())
-                    shiprect = ship.get_rect()
-                    shiprect = shiprect.move(mainShip.pos[0], mainShip.pos[1])
-                elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-                    mainShip.rotatecw(angle = -5.0)
-                    ship = rot_center(shipImage, mainShip.get_angle_from_vert())
-                    shiprect = ship.get_rect()
-                    shiprect = shiprect.move(mainShip.pos[0], mainShip.pos[1])
-            shiprect = shiprect.move(mainShip.speed[0], mainShip.speed[1])
-            mainShip.pos[0] += mainShip.speed[0]
-            mainShip.pos[1] += mainShip.speed[1]
-            if shiprect.left < 0 or shiprect.right > width:
-                mainShip.speed[0] = 0
-            if shiprect.top < 0 or shiprect.bottom > height:
-                mainShip.speed[1] = 0
-            screen.fill(black)
-            screen.blit(ship, shiprect)
+            keys = pygame.key.get_pressed()  #checking pressed keys
+            if keys[pygame.K_LEFT]:
+                self.rotShip(10)
+            if keys[pygame.K_RIGHT]:
+                self.rotShip(-10)
+            if keys[pygame.K_SPACE]:
+                self.accelerate()
+            self.moveShip()
+            #self.checkBounds()
+            self.gravity()
+            self.screen.fill(black)
+            self.screen.blit(self.ship, self.shiprect)
+            pygame.draw.circle(self.screen, (255, 0, 0), (int(self.width/2), int(self.height/2)), 10, 0)
             pygame.display.flip()
-            pygame.time.wait(20)
+            pygame.time.wait(33)
         pygame.display.quit()
         pygame.quit()
         sys.exit()
