d2c37880a3ff48edfd31ae003d5b40d90913ff45
CleanestMink126 <steelman@students.olin.edu>
2017-03-01 19:14:20 -0500

    added more blackhole

--- mainBoard.py
+++ mainBoard.py
@@ -5,6 +5,8 @@ import os
 import sys
 import pygame
 from pygame.locals import *
+import random
+import math
 
 if not pygame.font:
     print('Warning, fonts disabled')
@@ -13,42 +15,54 @@ if not pygame.mixer:
 
 
 class BoardInit:
-    def rotShip(self, myangle):
-        self.mainShip.rotatecw(angle=myangle)
-        self.ship = rot_center(self.shipImage, self.mainShip.get_angle_from_vert())
-        self.shiprect = self.ship.get_rect()
-        self.shiprect = self.shiprect.move(self.mainShip.pos[0], self.mainShip.pos[1])
+    def rotShip(self, myangle, shipNum):
+        self.ships[shipNum].rotatecw(angle=myangle)
+        self.shipImages[shipNum] = rot_center(self.shipImage, self.ships[shipNum].get_angle_from_vert())
+        self.shiprects[shipNum] = self.shipImages[shipNum].get_rect()
+        self.shiprects[shipNum] = self.shiprects[shipNum].move(self.ships[shipNum].pos[0], self.ships[shipNum].pos[1])
 
-    def accelerate(self):
-        self.mainShip.speed[0] -= (self.mainShip.direct[0])
-        self.mainShip.speed[1] -= (self.mainShip.direct[1])
+    def accelerate(self, shipNum):
+        self.ships[shipNum].speed[0] -= (self.ships[shipNum].direct[0])
+        self.ships[shipNum].speed[1] -= (self.ships[shipNum].direct[1])
 
-    def moveShip(self):
-        myTime = self.detTime()
-        xc = self.mainShip.speed[0]
-        yc = self.mainShip.speed[1]
-        self.shiprect = self.shiprect.move(int(xc * myTime), int(yc * myTime))
-        self.mainShip.pos[0] += int(xc * myTime)
-        self.mainShip.pos[1] += int(yc * myTime)
+    def moveShip(self, shipNum):
+        myTime = self.detTime(shipNum)
+        xc = self.ships[shipNum].speed[0]
+        yc = self.ships[shipNum].speed[1]
+        if shipNum == 0:
+            myTime = 1
+        self.shiprects[shipNum] = self.shiprects[shipNum].move(int(xc * myTime), int(yc * myTime))
+        self.ships[shipNum].pos[0] += int(xc * myTime)
+        self.ships[shipNum].pos[1] += int(yc * myTime)
 
-    def gravity(self):
-        distance = self.distanceCent()
-        self.mainShip.speed[0] -= ((self.mainShip.pos[0]-self.center[0])/distance * .3)
-        self.mainShip.speed[1] -= ((self.mainShip.pos[1]-self.center[1])/distance * .3)
+    def gravity(self, shipNum):
+        distance = self.distanceCent(shipNum)
+        self.ships[shipNum].speed[0] -= ((self.ships[shipNum].pos[0]-self.center[0])/distance * .2)
+        self.ships[shipNum].speed[1] -= ((self.ships[shipNum].pos[1]-self.center[1])/distance * .2)
 
-    def distanceCent(self):
-        return ((self.mainShip.pos[0]-self.center[0]) ** 2 + (self.mainShip.pos[1] - self.center[1]) ** 2) ** .5
+    def distanceCent(self, shipNum):
+        return ((self.ships[shipNum].pos[0]-self.center[0]) ** 2 + (self.ships[shipNum].pos[1] - self.center[1]) ** 2) ** .5
 
-    def detTime(self):
-        distance = self.distanceCent()
+    def detTime(self, shipNum):
+        distance = self.distanceCent(shipNum)
         print(distance)
         return distance / (distance + 500)
 
-    def checkBounds(self):
-        if self.shiprect.left < 0 or self.shiprect.right > self.width:
-            self.mainShip.speed[0] = 0
-        if self.shiprect.top < 0 or self.shiprect.bottom > self.height:
-            self.mainShip.speed[1] = 0
+    def checkBounds(self, shipNum):
+        if self.shiprects[shipNum].left < 0 or self.shiprects[shipNum].right > self.width:
+            self.ships[shipNum].speed[0] = 0
+        if self.shiprects[shipNum].top < 0 or self.shiprects[shipNum].bottom > self.height:
+            self.ships[shipNum].speed[1] = 0
+        if abs(self.shiprects[shipNum].top - self.center[1] < 50) or abs(self.shiprects[shipNum].bottom - self.center[1]) < 50:
+            self.ships.pop(shipNum)
+            self.shiprects.pop(shipNum)
+            self.shipImages.pop(shipNum)
+
+    def makeShip(self, tdirect = [0,1], tspeed = [0,0], tpos = [600,600]):
+        self.ships.append(shipClass.Ship(direct=tdirect, speed =tspeed, pos = tpos))
+        self.shipImages.append(self.shipImage.copy())
+        shiprect = self.ship.get_rect()
+        self.shiprects.append(shiprect.move(self.ships[len(self.ships)-1].pos))
 
     def __init__(self):
         pygame.init()
@@ -56,15 +70,22 @@ class BoardInit:
         black = 0, 0, 0
         self.screen = pygame.display.set_mode(self.size)
         # ball = pygame.draw.circle(self.screen, (255, 0, 0), (10, 10), 10, 0)
-        self.mainShip = shipClass.Ship(direct = [0,1], speed = [0,0], pos = [500,500])
         self.center = [self.width/2, self.height/2]
-
         self.shipImage = pygame.image.load(os.path.join('rocket.png'))
         self.shipImage = pygame.transform.scale(self.shipImage, (50, 50))
         self.shipImage = pygame.transform.rotate(self.shipImage, -45)
         self.ship = self.shipImage.copy()
         self.shiprect = self.ship.get_rect()
-        self.shiprect = self.shiprect.move(self.mainShip.pos)
+        self.ships = []
+        self.shipImages = []
+        self.shiprects = []
+        self.ships.append(shipClass.Ship(direct = [0,1], speed = [0,0], pos=[500,500]))
+        self.shiprect = self.shiprect.move(self.ships[0].pos)
+        self.shipImages.append(self.shipImage.copy())
+        self.shiprects.append(self.shiprect)
+        self.makeShip(tspeed=[2, 0])
+        self.makeShip(tpos=[100, 100])
+        self.makeShip()
         while 1:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -73,16 +94,21 @@ class BoardInit:
                     return
             keys = pygame.key.get_pressed()  #checking pressed keys
             if keys[pygame.K_LEFT]:
-                self.rotShip(10)
+                self.rotShip(10, 0)
             if keys[pygame.K_RIGHT]:
-                self.rotShip(-10)
+                self.rotShip(-10, 0)
             if keys[pygame.K_SPACE]:
-                self.accelerate()
-            self.moveShip()
-            #self.checkBounds()
-            self.gravity()
+                self.accelerate(0)
             self.screen.fill(black)
-            self.screen.blit(self.ship, self.shiprect)
+            for i in range(len(self.ships)):
+                self.moveShip(i)
+                self.gravity(i)
+                self.rotShip(0, i)
+                self.screen.blit(self.shipImages[i], self.shiprects[i])
+            while i < len(self.ships):
+                self.checkBounds(i)
+                i += 1
+
             pygame.draw.circle(self.screen, (255, 0, 0), (int(self.width/2), int(self.height/2)), 10, 0)
             pygame.display.flip()
             pygame.time.wait(33)
